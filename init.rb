9501114705b93b935888cc47fec7a5899badbf4e
Martin DENIZET <martin.denizet@supinfo.com>
2018-05-11 11:23:06 +0200

    Version bump to 0.1.1

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_custom_js do
   name 'Redmine Custom JS plugin'
   author 'Martin DENIZET'
   description 'Allows to insert custom JavaScript in Redmine'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/martin-denizet/redmine_custom_js'
   author_url 'http://martin-denizet.com'
 
